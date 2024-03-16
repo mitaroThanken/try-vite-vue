048b40bbe224745ca72c14ba3f4544dff47f1c7a
YAMAMI Taro <mitarothanken@gmail.com>
2024-03-16 20:23:14 +0900

    Clean up.

--- devcontainer-images-prebuild/prebuild.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-devcontainer build --workspace-folder typescript-node-with-java/ --image-name ghcr.io/mitarothanken/haproxy-with-minica/devcontainers/typescript-node-with-java:"$(date +%Y%m%d-%H%M)"
