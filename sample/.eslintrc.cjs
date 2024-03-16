10bb9f9e4004e967966d4eea011f94c85e4661d3
YAMAMI Taro <mitarothanken@gmail.com>
2024-03-16 15:23:15 +0000

    typed-linting
    cf. https://typescript-eslint.io/getting-started/typed-linting
    cf. https://eslint.vuejs.org/user-guide/#how-to-use-a-custom-parser
    cf. https://typescript-eslint.io/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
    cf. https://qiita.com/gone0021/items/998f4eeea38fe2119a57

--- sample/.eslintrc.cjs
+++ sample/.eslintrc.cjs
@@ -3,13 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  plugins: ['vue', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
     '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+    tsconfigRootDir: __dirname,
+    project: ['tsconfig.app.json', 'tsconfig.node.json', 'tsconfig.vitest.json'],
+    parser: '@typescript-eslint/parser'
+  },
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['./**/*.js', './**/*.jsx', './**/*.cjs', './**/*.mjs']
+    }
+  ]
 }
