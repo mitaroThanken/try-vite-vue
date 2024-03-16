4be726650ca11171c1d8bd58bd34ebce2ce374fc
YAMAMI Taro <mitarothanken@gmail.com>
2024-03-16 14:16:34 +0000

    Change ESLint rules - recommended

--- sample/.eslintrc.cjs
+++ sample/.eslintrc.cjs
@@ -3,10 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
