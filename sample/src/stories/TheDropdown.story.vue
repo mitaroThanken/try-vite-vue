9e403330dfca4dd778e22ca9e31d3e4009cd5336
YAMAMI Taro <mitarothanken@gmail.com>
2024-03-20 09:11:59 +0000

    [TRIAL] write stories

---
+++ sample/src/stories/TheDropdown.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { logEvent } from 'histoire/client';
+import TheDropdown from '@/components/TheDropdown.vue';
+
+const open = ref(false);
+
+const handleToggle = (current: boolean) => {
+  logEvent('toggle', { current });
+};
+</script>
+
+<template>
+  <Story>
+    <TheDropdown v-model:open="open" @toggle="handleToggle">
+      <template #summary>open or close</template>
+      <ul class="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+      </ul>
+    </TheDropdown>
+  </Story>
+</template>
---
+++ sample/src/stories/ThemeController.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ThemeController from '@/components/ThemeController.vue';
+</script>
+
+<template>
+  <Story>
+    <ThemeController />
+  </Story>
+</template>
