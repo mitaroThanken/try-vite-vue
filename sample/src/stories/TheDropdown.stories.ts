5fb4941437fd91d88a6ffb34a7fc1d9e090ac6b5
YAMAMI Taro <mitarothanken@gmail.com>
2024-03-20 11:26:11 +0000

    lit-html でストーリー内の Vue のテンプレートをそれっぽく整形できるようにする
    cf. https://qiita.com/oedkty/items/eceae8047166d5f252ad
    cf. https://marketplace.visualstudio.com/items?itemName=bierner.lit-html

--- sample/src/stories/TheDropdown.stories.ts
+++ sample/src/stories/TheDropdown.stories.ts
@@ -1,7 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import TheDropdown from './TheDropdown.vue';
+import TheDropdown from '@/components/TheDropdown.vue';
 import { useArgs } from '@storybook/preview-api';
 import { action } from '@storybook/addon-actions';
+
+// フォーマッターにこのテンプレートは HTML であると見せかけつつ
+// 実態は文字列のままとする
+const html = String.raw;
 
 const meta: Meta<typeof TheDropdown> = {
   component: TheDropdown,
@@ -25,62 +29,19 @@ export const Default: Story = {
         updateArgs({ onToggle: action('toggle') });
         return { args, updateOpen };
       },
-      template: `
-  <TheDropdown v-model:open="args.open" @toggle="args.onToggle" @update:open="updateOpen">
-    <template #summary>{{ args.summary }}</template>
-    <ul class="z-[1] w-52 rounded-box bg-base-300 p-2 shadow-2xl">
-      <li>
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-ghost btn-sm btn-block justify-start"
-          aria-label="Default"
-          value="default"
-        />
-      </li>
-      <li>
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-ghost btn-sm btn-block justify-start"
-          aria-label="Retro"
-          value="retro"
-        />
-      </li>
-      <li>
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-ghost btn-sm btn-block justify-start"
-          aria-label="Cyberpunk"
-          value="cyberpunk"
-        />
-      </li>
-      <li>
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-ghost btn-sm btn-block justify-start"
-          aria-label="Valentine"
-          value="valentine"
-        />
-      </li>
-      <li>
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-ghost btn-sm btn-block justify-start"
-          aria-label="Aqua"
-          value="aqua"
-        />
-      </li>
-    </ul>
-  </TheDropdown>
-    `,
+      template: html`
+        <TheDropdown v-model:open="args.open" @toggle="args.onToggle" @update:open="updateOpen">
+          <template #summary>{{ args.summary }}</template>
+          <ul tabindex="0" class="z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
+        </TheDropdown>
+      `,
     };
   },
   args: {
     open: false,
-    summary: 'Theme',
+    summary: 'open and close',
   },
 };
